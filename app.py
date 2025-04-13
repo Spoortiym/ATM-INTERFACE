115c0bce8a1dcc399f16a393a042437478ff4d05
Spoortiym <spoortiymugabasav@gmail.com>
2025-04-14 03:13:35 +0530

    Updated files

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="shreyapk",
+        password="#Spoorti8088"
         database="atm_db"
     )
 
